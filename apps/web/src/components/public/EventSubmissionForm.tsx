14474e83287dc2b63c58443bae096f427e9a8d49
kaixerrs <rusuhlemon@gmail.com>
2026-09-15 19:17:22 +0700

    fix(web): move use client directive to top of EventSubmissionForm

--- apps/web/src/components/public/EventSubmissionForm.tsx
+++ apps/web/src/components/public/EventSubmissionForm.tsx
@@ -1,5 +1,5 @@
-import dynamic from 'next/dynamic';
 "use client";
+import dynamic from 'next/dynamic';
 import { Toaster, toast } from 'react-hot-toast';
 
 import React, { useState, useRef, useEffect } from 'react';
