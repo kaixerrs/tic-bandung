c4f3ab84b802d689881831421cf72e180399f026
kaixerrs <rusuhlemon@gmail.com>
2026-09-22 22:09:11 +0700

    fix(admin): remove duplicated DeviceSessions component in profile layout

--- apps/admin/src/components/admin/AdminProfileForm.tsx
+++ apps/admin/src/components/admin/AdminProfileForm.tsx
@@ -196,7 +196,6 @@ export default function AdminProfileForm({ initialProfile }: { initialProfile: a
             </div>
           </form>
         </div>
-        <DeviceSessions />
       </div>
 
       {/* Password Section */}
